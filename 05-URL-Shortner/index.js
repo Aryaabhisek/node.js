d9bf9feb996b1a9463a728f977553a45150d13c8
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2025-12-07 23:48:09 +0530

    added 05-URL-Shortner

---
+++ 05-URL-Shortner/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRouter = require('./routes/url');
+const URL = require("./models/url");
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+.then(() => console.log("MongoDB Connected...")
+);
+
+
+
+// parse JSON bodies
+app.use(express.json());
+
+// mount the url router
+app.use("/url", urlRouter);
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+    {
+        shortId
+    }, 
+    {
+        $push: {
+        visitHistory: {
+            timestamp: Date.now(),
+        },
+    },
+    }
+);
+res.redirect(entry.redirectURL);
+});
+
+
+app.listen(PORT, () => 
+    console.log(`Server started at port : ${PORT}`)  //converted to string literals   
+)
