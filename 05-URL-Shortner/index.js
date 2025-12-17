891baebd698285fe1f8c91cff56a2becff48539e
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2025-12-18 00:32:17 +0530

    added new users model for auth

--- 05-URL-Shortner/index.js
+++ 05-URL-Shortner/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require('path')
 const { connectToMongoDB } = require("./connect");
 const urlRouter = require('./routes/url');
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 const app = express();
 const PORT = 8001;
@@ -17,33 +18,32 @@ app.set('views', path.resolve("./views"));
 
 // parse JSON bodies
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
-app.get("/test", async (req, res) => {
+/* app.get("/test", async (req, res) => {
     const allUrls = await URL.find({});
     return res.render('home',{
         urls: allUrls,
     });
-});
+}); */
 
 // mount the url router
 app.use("/url", urlRouter);
 
-app.get('/:shortId', async (req, res) => {
+app.use("/",staticRoute);
+
+app.get(['/:shortId', '/url/:shortId'], async (req, res) => {
     const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate(
-    {
-        shortId
-    }, 
-    {
-        $push: {
-        visitHistory: {
-            timestamp: Date.now(),
-        },
-    },
+    const updated = await URL.findOneAndUpdate(
+        { shortId },
+        { $push: { visitHistory: { timestamp: Date.now() } } },
+        { new: true }
+    );
+    if (!updated) {
+        return res.status(404).send('Short URL not found');
     }
-);
-res.redirect(entry.redirectURL);
+    return res.redirect(updated.redirectURL);
 });
 
 
---
+++ 05-URL-Shortner/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = required('mongoose')
+
+const userSchema = new mongoose.schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+    },
+    { timestamp: true }
+);
+
+
+const User = mongoose.model('user', userSchema)
+
+
+module.exports = User;
